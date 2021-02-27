d46a204888000e8d270a5862d33dd21ddb0a9ad5
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2021-02-27 17:52:37 +0100

    Make Fixtures a method of NITestCase

--- test.py
+++ test.py
@@ -3,18 +3,12 @@ from flask_testing import TestCase
 import unittest
 import main
 
-def fixtures():
-    obj1 = main.DataTable(key = "key1", value = "foo")
-    obj2 = main.DataTable(key = "key2", value = "bar")
-    main.db.session.add(obj1)
-    main.db.session.add(obj2)
-    main.db.session.commit()
-
-
+# Child of TestCase and Parent to all Test Clases
 class NITestCase(TestCase):
 
     def create_app(self):
         app = main.app
+        # Creates a testing database(that runs in RAM)
         app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
         app.config['TESTING'] = True
         return app
@@ -25,6 +19,14 @@ class NITestCase(TestCase):
     def tearDown(self):
         main.db.session.remove()
         main.db.drop_all()
+
+    # Populates the testing database
+    def fixtures(self):
+        obj1 = main.DataTable(key = "key1", value = "foo")
+        obj2 = main.DataTable(key = "key2", value = "bar")
+        main.db.session.add(obj1)
+        main.db.session.add(obj2)
+        main.db.session.commit()
 
 
 class TestIndex(NITestCase):
@@ -41,27 +43,29 @@ class TestGetAll(NITestCase):
         self.assertEqual(response.json, [])
 
     def test_db_populated(self):
-        fixtures()
+        self.fixtures()
         response = self.client.get("/keys")
         self.assertEqual(response.json, [{"key1": "foo"}, {"key2": "bar"}])
+
 
 class TestGetValue(NITestCase):
 
     def test_value_existent(self):
-        fixtures()
+        self.fixtures()
         response = self.client.get("/keys/key2")
         self.assertEqual(response.json, "bar")
 
     def test_value_unexistent(self):
-        fixtures()
+        self.fixtures()
         response = self.client.get("/keys/key3")
         self.assertEqual(response.json, "Not Found")
         self.assert404(response)
 
+
 class TestDeleteAll(NITestCase):
 
     def test_delete_all(self):
-        fixtures()
+        self.fixtures()
         response = self.client.delete("/keys")
         self.assertEqual(response.json, "Deleting all database entries")
         self.assert200(response)
